9a23521e229f32b159d5337bf7bfe077e1a21ee7
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-06-09 18:37:37 +0530

    Dat 13

---
+++ Hashing/Count pairs with given sum.cpp
@@ -0,0 +1,14 @@
+// Problem Link : https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1#
+    int getPairsCount(int arr[], int n, int k) {
+        // we will maintain count thai which elements appears how many times before current position
+        // so for current element our required element would be k - arr[i] 
+        // so we will increment answer by count of k-arr[i] 
+        // because current element can make a pair with all element which are equal to k-arr[i]
+        int answer = 0;
+        unordered_map<int,int> track;
+        for(int i = 0; i < n; i++){
+            answer += track[k - arr[i]];
+            track[arr[i]]++;
+        }
+        return answer;
+    }
