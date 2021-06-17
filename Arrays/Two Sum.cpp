bed66c1e74a657f8d2f2197bb558a03284a1ca9e
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-06-17 16:55:30 +0530

    Day 20

--- Arrays/Two Sum.cpp
+++ Arrays/Two Sum.cpp
@@ -5,7 +5,7 @@
             if(mp.count(target-nums[i]) > 0){ // if complement is present then we will return index of both
                 vector<int> ans;
                 ans.push_back(mp[target-nums[i]]);
-                ans.push_back(i);
+                ans.push_back(i); 
                 return ans;
             }else{ // else we will add that element in the map
                 mp[nums[i]] = i;
