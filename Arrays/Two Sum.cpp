a299b148d7c65e95fab894993d749e8d9d5f84a7
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-06-17 16:50:46 +0530

    Create Two Sum.cpp

---
+++ Arrays/Two Sum.cpp
@@ -0,0 +1,15 @@
+// Problem Link : https://leetcode.com/problems/two-sum/submissions/
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.count(target-nums[i]) > 0){ // if complement is present then we will return index of both
+                vector<int> ans;
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }else{ // else we will add that element in the map
+                mp[nums[i]] = i;
+            }
+        }
+        return nums;
+    }
