89fe5d4e77380c310457a2869868ea00792bdd0a
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-07-22 23:15:42 +0530

    Harbour.Space Scholarship Contest 2021-2022 (Div.1 + Div.2)

---
+++ CodeForces/A - Digits Sum.cpp
@@ -0,0 +1,17 @@
+// Problem Link : https://codeforces.com/contest/1553/problem/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    for (int z = 1; z <= t; z++){
+        int n;
+        cin >> n;
+
+        cout << (n+1)/10 << endl;
+    }
+    return 0;
+}
