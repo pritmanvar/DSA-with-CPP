196d3c84d190072251fe2f3d2d5a721ef9e7fc2b
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2021-10-03 19:32:29 +0530

    Codeforces Round #411 (Div. 2)

---
+++ CodeForces/A. Fake NP.cpp
@@ -0,0 +1,16 @@
+// Problem Link : https://codeforces.com/problemset/problem/805/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    int n,m;
+    cin >> n >> m;
+    if(n != m)
+        cout << 2 << endl;
+    else
+        cout << n << endl;
+    return 0;
+}
