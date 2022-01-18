9488f541d01cdb7a5fdb0113e3d2a3aed5e57b75
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-01-18 21:05:13 +0530

    Day 115

--- Arrays/Count the subarrays having product less than k.cpp
+++ Arrays/Count the subarrays having product less than k.cpp
@@ -1,37 +1,32 @@
-// problem Link : https://practice.geeksforgeeks.org/problems/ugly-numbers2254/1/
+// Problem Link : https://practice.geeksforgeeks.org/problems/count-the-subarrays-having-product-less-than-k1708/1/
 class Solution{
-public:	
-	// #define ull unsigned long long
-	/* Function to get the nth ugly number*/
-	ull getNthUglyNo(int n) {
-	    vector<ull> ugly;
-	    unordered_set<ull> st;
-	    ugly.push_back(1);
-	    st.insert(1);
-	    int i2 = 0, i3 = 0, i5 = 0; 
-	    
-	    while(ugly.size() < n){
-	        if(ugly[i2]*2 <= ugly[i3]*3 && ugly[i2]*2 <= ugly[i5]*5){
-	            if(st.count(ugly[i2]*2) == 0){
-	                ugly.push_back(ugly[i2]*2);
-	            }
-	            st.insert(ugly.back());
-	            i2++;
-	        }else if(ugly[i3]*3 <= ugly[i2]*2 && ugly[i3]*3 <= ugly[i5]*5){
-	            if(st.count(ugly[i3]*3) == 0){
-	                ugly.push_back(ugly[i3]*3);
-	                
-	            }
-	            st.insert(ugly.back());
-	            i3++;
-	        }else{
-	            if(st.count(ugly[i5]*5) == 0){
-	                ugly.push_back(ugly[i5]*5);
-	            }
-	            st.insert(ugly.back());
-	            i5++;
-	        }
-	    }
-	    return ugly[n-1];
-	}
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long mul = 1;
+        int ans = 0;
+        int st = 0, ed = 0;
+        
+        
+        while(ed < n){
+            // valid window
+            while(ed < n && mul*a[ed] < k){
+                mul *= a[ed];
+                ed++;
+            }
+            
+            if(ed == st){ // corner case
+                ed++,st++;
+                continue;
+            }
+            ans += ed-st; // number of subarrays in this valid window who ends with ed.
+            mul /= a[st];
+            st++;
+        }
+        while(st < ed){ // reduce window.
+            ans += ed-st;
+            mul /= a[st];
+            st++;
+        }
+        return ans;
+    }
 };
