6ea35f0662f80640f2e9cf595d64c73e360f0dcc
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-03-03 19:14:03 +0530

    Day 199

---
+++ Striver CP Sheet/A - Chewbaсca and Number.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main() 
+{
+    ll n;
+    cin >> n;
+    
+    string ans = "";
+    while(n > 0){
+        if(n%10 < 5){
+            ans += to_string(n%10);
+        }else{
+            ans += to_string(9 - n%10);
+        }
+        n /= 10;
+    }
+    reverse(ans.begin(),ans.end());
+    if(ans[0] == '0'){
+        ans[0] = '9';
+    }
+    cout << stoll(ans) << endl;
+    return 0;
+}
