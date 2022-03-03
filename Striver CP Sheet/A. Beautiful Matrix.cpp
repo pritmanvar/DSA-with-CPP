6a11624411e2ba5cef7e61cba55caaa1bb995002
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-03-03 19:15:09 +0530

    Day 199

---
+++ Striver CP Sheet/A. Beautiful Matrix.cpp
@@ -0,0 +1,27 @@
+// Problem Link : https://codeforces.com/problemset/problem/263/A
+
+// index of cell at center will be 2,2 so minimum moves required is abs(i-2)+abs(j-2)
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int ans = 0;
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            int temp;
+            cin >> temp;
+
+            if(temp == 1){
+                ans = abs(2-i)+abs(2-j);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
