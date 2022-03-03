59fbad023066f6f627a749167089ae0ee99a48b7
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-03-03 19:16:13 +0530

    Day 199

---
+++ Striver CP Sheet/A. Soft Drinking.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,k,l,c,d,p,nl,np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+
+    int totalLiter = k*l, limes = c*d;
+    cout << min(totalLiter/nl,min(limes,p/np))/n << endl;
+    return 0;
+}
