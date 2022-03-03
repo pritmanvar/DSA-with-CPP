1cbf8c4aa1fb3432f52e6d8c85d5f9d0818462ca
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-03-03 19:12:51 +0530

    Day 199

---
+++ Striver CP Sheet/A - Bit++.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int z = 1; z <= n; z++){
+        string operation;
+        cin >> operation;
+
+        if(operation[1] == '+')
+            ans++;
+        else
+            ans --;
+    }
+    cout << ans << endl;
+    return 0;
+}
