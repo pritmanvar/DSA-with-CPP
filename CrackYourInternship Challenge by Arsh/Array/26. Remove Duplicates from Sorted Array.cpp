340ddd453e26398907bd8caba8c6ba5bb8a10dbc
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-05-10 17:10:08 +0530

    Day 129

---
+++ CrackYourInternship Challenge by Arsh/Array/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,13 @@
+// Problem Link : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++){
+            if(nums[i] != nums[j]){
+                nums[++i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
