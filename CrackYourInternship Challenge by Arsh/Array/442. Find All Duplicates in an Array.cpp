ea1e55ce48e0f53faaa2f235fb42b12efc0ae2d5
Prit Manvar <77919675+pritmanvar@users.noreply.github.com>
2022-05-11 16:56:05 +0530

    Day 130

---
+++ CrackYourInternship Challenge by Arsh/Array/442. Find All Duplicates in an Array.cpp
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/find-all-duplicates-in-an-array/
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[abs(nums[i])-1] < 0){
+                ans.push_back(abs(nums[i]));
+            }else{
+                nums[abs(nums[i])-1] = -nums[abs(nums[i])-1];
+            }
+        }
+        return ans;
+    }
+};
